d3fcfb26e7af30e8a03ea88b3cfa7eebe818a7c4
bilokonn <bilokonn@tests-MacBook-Air-2.local>
2026-04-19 01:07:12 +0300

    lesson-3 homework

---
+++ lesson-3/chalk-version-5.6.2/nodeBasics/index.js
@@ -0,0 +1,8 @@
+import chalk from 'chalk';
+
+const testGreenText = 'Green text';
+const testBlueText = 'Blue text';
+
+
+console.log(chalk.green(testGreenText));
+console.log(chalk.blue(testBlueText));
