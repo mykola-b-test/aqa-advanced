e68f7e6d25e62b44460d14e5377acc8986721ed5
bilokonn <bilokonn@tests-MacBook-Air-2.local>
2026-04-24 15:41:33 +0300

    lesson-4 homework

---
+++ lesson-4/jsBasics/task2.js
@@ -0,0 +1,8 @@
+
+let person1 = 'Jane Wilhelm';
+let person2 = 'Alex Brendon';
+
+let resultGreeting1 = "Dear " + person1 + " and " + person2 + ", welcome to the my birthday party!";
+let resultGreeting2 = `Dear ${person1} and ${person2}, nice to see you at the my birthday party!`;  
+console.log(resultGreeting1);
+console.log(resultGreeting2);
---
+++ lesson-4/jsBasics/task4.js
@@ -0,0 +1,22 @@
+// task 4.1
+{
+    let radius = 10;
+    let circleSquare = Math.PI * radius ** 2;
+    console.log((circleSquare).toFixed(2));
+}
+
+// task 4.2 
+{
+    let length = 2.34;
+    let width = 1.45;
+    let rectangleSquare = length * width;
+    console.log((rectangleSquare).toFixed(2));
+}
+
+// task 4.3
+{
+    let radius = 5.99;
+    let height = 6.32;
+    let cylinderVolume = Math.PI * radius ** 2 * height;
+    console.log((cylinderVolume).toFixed(2));
+}
